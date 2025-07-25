df3c4e27df9d32e257967a8bd52bd433e89d034b
daktollo <mevlutcanadiyanan@gmail.com>
2025-07-25 22:31:28 +0300

    feat: add password hashing and UUID generation for User class; include bcrypt and uuid dependencies

--- user.js
+++ user.js
@@ -1,17 +1,22 @@
-class User{
-	constructor(name, age, email) {
-		this.name = name;
-		this.age = age;
-		this.email = email;
-		this.purchases = [];
-	}
+const { v4: uuidv4 } = require('uuid');
+const Cart = require('./cart');
+const { hashPassword } = require('./passwordHelper');
 
-	buyProduct(product) {
-		this.purchases.push(product);
-	}
+class User {
+    constructor(name, age, email, password) {
+        this.id = uuidv4();
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.password = hashPassword(password);
+        this.purchases = [];
+        this.cart = new Cart();
+    }
 
+
+    buyProduct(product) {
+        this.purchases.push(product);
+    }
 }
-
-
 
 module.exports = User;
